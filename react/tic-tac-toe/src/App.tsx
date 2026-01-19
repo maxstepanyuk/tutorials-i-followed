bc057a3dab43a8b47f492201b7a63d0d710f3b7a
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2026-01-19 19:29:19 +0200

    react/tic-tac-toe: move declarations of regular functions ti be inline in order reduce boilerplate

--- react/tic-tac-toe/src/App.tsx
+++ react/tic-tac-toe/src/App.tsx
@@ -16,33 +16,22 @@ export default function Board() {
     setSquares(nextSquares);
   }
 
-  function handleClick0() { handleClick(0) }
-  function handleClick1() { handleClick(1) }
-  function handleClick2() { handleClick(2) }
-  function handleClick3() { handleClick(3) }
-  function handleClick4() { handleClick(4) }
-  function handleClick5() { handleClick(5) }
-  function handleClick6() { handleClick(6) }
-  function handleClick7() { handleClick(7) }
-  function handleClick8() { handleClick(8) }
-
-
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick0} />
-        <Square value={squares[1]} onSquareClick={handleClick1} />
-        <Square value={squares[2]} onSquareClick={handleClick2} />
+        <Square value={squares[0]} onSquareClick={function handleClick0() { handleClick(0) }} />
+        <Square value={squares[1]} onSquareClick={function handleClick1() { handleClick(1) }} />
+        <Square value={squares[2]} onSquareClick={function handleClick2() { handleClick(2) }} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={handleClick3} />
-        <Square value={squares[4]} onSquareClick={handleClick4} />
-        <Square value={squares[5]} onSquareClick={handleClick5} />
+        <Square value={squares[3]} onSquareClick={function handleClick3() { handleClick(3) }} />
+        <Square value={squares[4]} onSquareClick={function handleClick4() { handleClick(4) }} />
+        <Square value={squares[5]} onSquareClick={function handleClick5() { handleClick(5) }} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={handleClick6} />
-        <Square value={squares[7]} onSquareClick={handleClick7} />
-        <Square value={squares[8]} onSquareClick={handleClick8} />
+        <Square value={squares[6]} onSquareClick={function handleClick6() { handleClick(6) }} />
+        <Square value={squares[7]} onSquareClick={function handleClick7() { handleClick(7) }} />
+        <Square value={squares[8]} onSquareClick={function handleClick8() { handleClick(8) }} />
       </div>
     </>
   )
