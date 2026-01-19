18b53f8c310e7bbda18262e441d5138f712f42b4
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2026-01-19 19:16:07 +0200

    react/tic-tac-toe: create regular functions to be passed/used by each `Square`. (as a test)

--- react/tic-tac-toe/src/App.tsx
+++ react/tic-tac-toe/src/App.tsx
@@ -16,22 +16,33 @@ export default function Board() {
     setSquares(nextSquares);
   }
 
+  function handleClick0() { handleClick(0) }
+  function handleClick1() { handleClick(1) }
+  function handleClick2() { handleClick(2) }
+  function handleClick3() { handleClick(3) }
+  function handleClick4() { handleClick(4) }
+  function handleClick5() { handleClick(5) }
+  function handleClick6() { handleClick(6) }
+  function handleClick7() { handleClick(7) }
+  function handleClick8() { handleClick(8) }
+
+
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={handleClick0} />
+        <Square value={squares[1]} onSquareClick={handleClick1} />
+        <Square value={squares[2]} onSquareClick={handleClick2} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={handleClick3} />
+        <Square value={squares[4]} onSquareClick={handleClick4} />
+        <Square value={squares[5]} onSquareClick={handleClick5} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={handleClick6} />
+        <Square value={squares[7]} onSquareClick={handleClick7} />
+        <Square value={squares[8]} onSquareClick={handleClick8} />
       </div>
     </>
   )
