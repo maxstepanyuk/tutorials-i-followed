13982dfd11a5c15cd9bf60469baee56c2b9c35f4
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2026-01-21 22:19:40 +0200

    react/tic-tac-toe: add `calculateWinner()`

--- react/tic-tac-toe/src/App.tsx
+++ react/tic-tac-toe/src/App.tsx
@@ -6,6 +6,35 @@ function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
+function calculateWinner(squares: []) {
+  const lines_winnerIndexes = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let index = 0; index < lines_winnerIndexes.length; index++) {
+
+    const [a, b, c] = lines_winnerIndexes[index];
+    if (
+      squares[a] &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c]
+    ) {
+      return squares[a]
+    }
+
+    return null;
+  }
+
+  return null;
+}
+
 export default function Board() {
 
   const [squares, setSquares] = useState(Array(9).fill(null));
