1f4e1e7ad36c076fef9dc386f3910d4a2490f2a5
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2026-02-03 08:56:34 +0200

    react/thinking-in-react: `App()` return static HTML mockup

--- react/thinking-in-react/src/App.tsx
+++ react/thinking-in-react/src/App.tsx
@@ -12,6 +12,30 @@ function App() {
 
   return (
     <>
+      <div>
+        <form action="">
+          <input type="text" />
+          <input type="checkbox" name="" id="inStockOnly" />
+          <label htmlFor="inStockOnly">Only show products in stock</label>
+        </form>
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr><th>Name</th><th>Price</th></tr>
+          </thead>
+          <tbody>
+            <tr><td colSpan={2}>Fruits</td></tr>
+            <tr><td>Apple</td><td>$1</td></tr>
+            <tr><td>Dragonfruit</td><td>$1</td></tr>
+            <tr><td className="not-stocked">Passionfruit</td><td>$2</td></tr>
+            <tr><td colSpan={2}>Vegetables</td></tr>
+            <tr><td>Spinach</td><td>$2</td></tr>
+            <tr><td className="not-stocked">Pumpkin</td><td>$4</td></tr>
+            <tr><td>Peas</td><td>$1</td></tr>
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
